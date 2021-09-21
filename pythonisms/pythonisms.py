d260ea77d5b6358494e7995600d9dcffe3842c1f
dina-fouad <dina_albarghouthi@yahoo.com>
2021-09-21 17:00:05 +0300

    clss41

--- pythonisms/pythonisms.py
+++ pythonisms/pythonisms.py
@@ -1,69 +1,28 @@
 from functools import wraps
 
-class Node:
-  def __init__(self, value, next=None):
-    self.value = value
-    self.next = next
 
-class LinkedList:
+def invocation_log(func):
+    @wraps(func)
+    def inner_func(*args, **kwargs):
+        print(f'Before Calling {func.__name__}')
+        func(*args, **kwargs)
+        print(f'After Calling {func.__name__}')
 
-    def __init__(self, collection=None):
-        self.head = None
+    return inner_func
 
-        if collection:
-            for item in reversed(collection):
-                self.insert(item) 
 
-    def __iter__(self):
-     def generator():
-      current = self.head
-      while current:
-        yield current.value
-        current = current.next
-     return generator()
-
-    def __str__(self):
-        out = ""
-        for value in self:
-            out += f"[ {value} ] -> "
-        return out + "None"
-
-    def __len__(self):
-        return len(list(iter(self)))
-
+@invocation_log
+def say_hello(name):
+    """Say hello to someone"""
+    print(f"Hello, {name}!")
    
-
-    def __getitem__(self, index):
-        if index < 0:
-            index = len(self) + index
-
-        for i, item in enumerate(self):
-            if i == index:
-                return item
-        raise IndexError
-
-
-    def insert(self, value):
-        self.head = Node(value, self.head)
-
-    def append(self, value):
-
-        node = Node(value)
-
-        if not self.head:
-            self.head = node
-            return
-
-        current = self.head
-
-        while current.next:
-            current = current.next
-            current.next = node
-
-    def proclaim(func):
-      @wraps(func)
-      def wrapper(*args, **kwargs):
-        orig_val = func(*args, **kwargs)
-        return "On this day I do say, " + orig_val
-
-      return wrapper
+@mydeco
+def add(a, b):
+    return a + b
+@mydeco
+def mysum(*args):
+    
+    total = 0
+    for one_item in args:
+        total += one_item
+    return total
